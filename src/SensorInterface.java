555bdcf7e92580cf0e448184efddd9337091207c
billythemoose <mdiegothomas@gmail.com>
2019-06-03 19:46:29 -0700

    making code reviews easy is hard

--- src/SensorInterface.java
+++
@@ -1,9 +0,0 @@
-public interface SensorInterface {
-    double value();
-    int percentValue();
-    String report();
-    String name();
-    String getUnits();
-    double getMin();
-    double getMax();
-}
